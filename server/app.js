a249fd7fe7ac6612d88c64f1aa8f6f68f3712dae
Algefmarc Anthony L. Almocera <algefalmocera@gmail.com>
2016-07-19 15:38:44 +0800

    Handle connection.init()

--- server/app.js
+++ server/app.js
@@ -1,3 +1,16 @@
 console.log('You are running sequelize-sample');
 
 var config = require('./config/config');
+var connection = require('./database/connection');
+
+
+// init
+connection.init().then(function(response) {
+    
+    // do something after database was inialized
+    // like starting crons and other stuffs.. =D
+
+}, function(error) {
+    console.log('Error initializing database connection:');
+    console.log(error);
+});

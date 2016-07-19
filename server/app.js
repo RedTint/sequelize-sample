1f5a690f912116954538dbb3ae4d2727b7da8f6d
Algefmarc Anthony L. Almocera <algefalmocera@gmail.com>
2016-07-19 18:10:06 +0800

    Create sample for inserting data

--- server/app.js
+++ server/app.js
@@ -9,6 +9,24 @@ connection.init().then(function(response) {
     
     // do something after database was inialized
     // like starting crons and other stuffs.. =D
+    console.log('> Done initializing mysql connection')
+    
+    // executing sample insertation of data
+    var testRepo = require('./database/repositories/test-repository');
+    
+    testRepo.insert({
+        description: 'Test Description'
+    }).then(function(result) {
+
+        console.log('> Showing sequelize insert response');
+        console.log(result);
+
+    }, function(error){ 
+
+        console.log('> An error occured while inserting data:');
+        console.log(error);
+        
+    });
 
 }, function(error) {
     console.log('Error initializing database connection:');
